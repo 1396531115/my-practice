ba4dfe1b7f023a18bf78b7b386e08905f65907d2
w.shawn <1124170486@qq.com>
2019-12-04 21:51:56 +0800

    just for practice

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "./assets/styles/index.scss";
+import "./assets/styles/public.scss";
+
+ReactDOM.render(<h1>hellow world!</h1>, document.getElementById('app'));
+
---
+++ webpack.config.js
@@ -0,0 +1,86 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin"); // 打包HTML文件
+const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // 单独打包CSS文件
+const OptimizeCssPlugin = require("optimize-css-assets-webpack-plugin"); // 压缩CSS文件
+const UglifyjsPlufin = require("uglifyjs-webpack-plugin"); // 压缩JS文件
+const { CleanWebpackPlugin } = require("clean-webpack-plugin"); // 清除上次打包的文件
+
+const devMod = process.env.NODE_ENV !== 'production';
+
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.tsx", 
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/[name].[hash].js"
+  },
+  devServer: {
+    contentBase: './dist',
+    compress: true,
+    open: true,
+    port: 9527
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx$/,
+        include: path.resolve(__dirname, "src"),
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/react"],
+            cacheDirectory: true,
+          }
+        }
+      },
+      {
+        test: /\.(c|sc)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: devMod,
+              publicPath: '../',
+              reloadAll: true
+            }
+          },
+          "css-loader",
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 10000,
+              name: 'images/[name].[ext]',
+            }
+          }
+        ]
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html"
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: devMod ? 'css/[name].css' : 'css/[name].[hash].css',
+      chunkFilename: devMod ? '[id].css' : '[id].[hash].css',
+    })
+  ],
+  optimization: {
+    minimizer: [
+      new UglifyjsPlufin({
+        test: /\.js(\?.*)?$/i,
+        cache: true,
+        parallel: true
+      }),
+      new OptimizeCssPlugin()
+    ]
+  }
+}
