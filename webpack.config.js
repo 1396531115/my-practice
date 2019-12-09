7760003dc0b86c8cfa5d5e1561a4aeaade3f50aa
w.shawn <1124170486@qq.com>
2019-12-09 22:59:07 +0800

    add babelrc

--- webpack.config.js
+++ webpack.config.js
@@ -38,7 +38,6 @@ module.exports = {
         use: {
           loader: "babel-loader",
           options: {
-            presets: ["@babel/preset-env", "@babel/react"],
             cacheDirectory: true,
           }
         }
